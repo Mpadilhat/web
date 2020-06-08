ce568bcc79cd52de10e917042a3fad9f558b9e7c
Marcos Vinícius de Souza Padilha <53908142+Mpadilhat@users.noreply.github.com>
2020-06-08 01:43:47 -0300

    centralizei o conteudo home (#1)

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -3,8 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>HOME</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis non blanditiis assumenda neque iusto nemo, earum deleniti sit nisi iure eveniet itaque porro consequuntur aperiam molestias dignissimos voluptate delectus!</p>
+      <center>
+        <h1>HOME</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis non blanditiis assumenda neque iusto nemo, earum deleniti sit nisi iure eveniet itaque porro consequuntur aperiam molestias dignissimos voluptate delectus!</p>
+      </center>
     </div>
   );
 }
