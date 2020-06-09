9bafbf0787e2728e1ecca1882155eb6115d6af5e
Maria Eduarda Kovalski <45320287+kovalskiduda@users.noreply.github.com>
2020-06-08 21:38:16 -0300

    tela de login (#3)
    excelente! :)

---
+++ src/screens/login/login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { icons, images } from "../../assets";
+import * as s from "./styled-login";
+
+const Login = () => {
+  return (
+    <s.Body >
+      <s.Container>
+        <s.Box>
+          <s.Line>
+            <s.Image src={icons.logo} />
+            <s.Image src={icons.title} width={"350px"} height={"280px"}/>
+          </s.Line>
+          <s.Input placeholder="E-mail" />
+          <s.Input placeholder="Senha" />
+          <s.Button>Entrar</s.Button>
+        </s.Box>
+      </s.Container>
+    </s.Body>
+  );
+}
+
+export default Login;
