e1c2a2164e2309eebf668460c24068ce6af8d5d2
Marcos Vinícius de Souza Padilha <mpadilhat@gmail.com>
2020-06-15 19:17:05 -0300

    adição de icones

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,15 +2,18 @@ import logo from "./images/logo.png";
 import title from "./images/title.png";
 import logotipo from "./icons/logotipo.svg";
 import mail from "./icons/mail.svg";
-import entrar from "./icons/entrar.svg";
 import lock from "./icons/lock.svg";
+import empresa from "./icons/empresa.svg";
+import fone from "./icons/fone.svg";
+import cnpj from "./icons/cnpj.svg";
 
 const icons = {
   logotipo,
-  title,
   mail,
-  entrar,
   lock,
+  empresa,
+  fone,
+  cnpj,
 };
 
 const images = {
