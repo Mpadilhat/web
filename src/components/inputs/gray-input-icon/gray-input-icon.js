b77a8edc43851d8ff1fa4c8b0eff3a8dd1962361
Maria Eduarda Kovalski <eduardakovalski@gmail.com>
2020-06-15 22:28:34 -0300

    cadastro unica tela

--- src/components/inputs/gray-input-icon/gray-input-icon.js
+++ src/components/inputs/gray-input-icon/gray-input-icon.js
@@ -1,11 +1,21 @@
 import React from "react";
 import * as s from "./styled-gray-input-icon";
 
-export default ({ value, onChange, placeholder, src, type, width, height }) => {
+export default ({
+  value,
+  onChange,
+  placeholder,
+  src,
+  type,
+  width,
+  height,
+  margin,
+}) => {
   return (
-    <s.DivInput>
+    <s.DivInput margin={margin}>
       <s.Icon src={src} />
       <s.Input
+        src={src}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
