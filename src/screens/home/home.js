688b7820380fb229be089fb7362f4ad1b87eefc9
Marcos Vinícius de Souza Padilha <mpadilhat@gmail.com>
2020-06-16 18:19:41 -0300

    header e responsividade da home

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,14 +1,23 @@
 import React from "react";
+import * as s from "./styled-home";
+import { Header } from "../../components";
 
 const Home = () => {
   return (
-    <div>
-      <center>
+    <s.Body>
+      <Header />
+
+      <s.Container>
         <h1>HOME</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis non blanditiis assumenda neque iusto nemo, earum deleniti sit nisi iure eveniet itaque porro consequuntur aperiam molestias dignissimos voluptate delectus!</p>
-      </center>
-    </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          perferendis non blanditiis assumenda neque iusto nemo, earum deleniti
+          sit nisi iure eveniet itaque porro consequuntur aperiam molestias
+          dignissimos voluptate delectus!
+        </p>
+      </s.Container>
+    </s.Body>
   );
-}
+};
 
 export default Home;
---
+++ src/screens/home/styled-home.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  background-color: #fad246;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Container = styled.div`
+  background-color: #fad246;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
