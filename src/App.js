92c3d6eef40de21478c164ca4f998102e6220fb7
Uéslei Suptitz <54419037+uesleisuptitz@users.noreply.github.com>
2020-06-16 20:12:06 -0300

    Feature/teste (#9)
    * Teste
    
    * Teste

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import GlobalStyles from "./global-style/globalStyles";
 import { Home, Login, Register } from "./screens";
 
 function App() {
+//Uéslei esteve aqui
   return (
     <div style={{ height: "100vh", flex: 1 }}>
       <GlobalStyles />
       <Router>
         <Switch>
-          <Route path="/login" exact component={() => <Login />} />
-          <Route path="/register" exact component={() => <Register />} />
-          <Route path="/" exact component={() => <Home />} />
+          {/* <Route path="/login" exact component={() => <Login />} /> */}
+          {/* <Route path="/register" exact component={() => <Register />} /> */}
+          {/* <Route path="/" exact component={() => <Home />} /> */}
         </Switch>
       </Router>
     </div>
